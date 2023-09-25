4c426d0aaacde61ae381228bed8164cad128085b
jh0147 <jh0147@hotmail.com>
2023-09-25 11:29:43 +0900

    21

---
+++ src/main/java/com/example/todo/persistence/TodoRepository.java
@@ -0,0 +1,12 @@
+package com.example.todo.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.todo.model.TodoEntity;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String>{
+
+}
+//스프링 jpa가 알아서해줌 save...등
